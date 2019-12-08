240ab93eb6a5ec192203f72b90628ab3c1f9ca48
Alexander Dimopoulos <andimo11@gmail.com>
2019-12-07 16:42:40 -0800

    Initial Commit

---
+++ iSight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iSight
+//
+//  Created by Alexander N. Dimopoulos on 12/7/19.
+//  Copyright © 2019 Alexander N. Dimopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
